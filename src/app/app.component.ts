686650c9dad53e5dfc408ded5b4070aed446421d
Rafael Amorim <araujor920@gmail.com>
2025-01-05 10:48:21 -0300

    [FIX] navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,
+    MatToolbarModule
+  ],
   templateUrl: './app.component.html',
   styles: [`
     nav {
