7e539a6819713a279919df9d0ce00f19b4ebaed2
Rafael Amorim <araujor920@gmail.com>
2025-01-11 18:43:58 -0300

    footer changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule,
+    CommonModule,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+
+}
--- src/app/componentes/history/history.component.ts
+++ src/app/componentes/history/history.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
